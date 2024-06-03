b83db022253a15657c164f94eaae764d55278158
DavidPrinz1123 <david.prinz1123@gmail.com>
2024-06-03 16:11:17 +0200

    changed desources root folder

--- src/main/maps/PistonDam.java
+++ src/main/maps/PistonDam.java
@@ -2,15 +2,20 @@ package main.maps;
 
 public class PistonDam {
     public static final String[][] LAYOUT = {
-            {"water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water"},
-            {"water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "grass", "wood", "grass", "grass", "grass", "wood", "water"},
-            {"water", "water", "water", "mountain water", "water", "water", "water", "water", "grass", "turn right down", "street h", "street h", "street h", "street h", "street h", "street h", "little village", "grass", "water"},
-            {"water", "water", "water", "water", "water", "water", "water", "mountain", "grass", "street v", "grass", "wood", "grass", "grass", "grass", "grass", "grass", "grass", "water"},
-            {"water", "water", "water", "water", "water", "wood", "grass", "wood", "wood", "street v", "grass", "grass", "grass", "wood", "grass", "wood", "grass", "water", "water"},
-            {"water", "water", "wood", "grass", "grass", "grass", "wood", "grass", "grass", "street v", "wood", "wood", "wood", "grass", "water", "water", "water", "water","water"},
-            {"water", "grass", "grass", "grass", "grass", "grass", "wood", "wood", "grass", "street v", "grass", "mountain", "water", "water", "water", "water", "water", "water", "water"},
-            {"water", "grass", "little village", "street h", "street h", "street h", "street h", "street h", "street h", "turn left up", "grass", "water", "water", "mountain water", "water", "water", "water", "water", "water"},
-            {"water", "wood", "grass", "grass", "grass", "wood", "grass", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water"},
-            {"water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water", "water"}
+            {"grass","grass","grass", "wood", "wood", "grass", "grass", "grass", "grass", "grass","grass", "wood", "wood", "wood", "wood", "grass", "grass", "grass","grass", "grass", "grass", "wood", "wood", "grass", "grass", "grass"},
+            {"wood","grass","little village","street h", "street h", "street h", "street h", "street h", "street h", "street h","street h", "street h", "street h", "street h", "street h", "street h", "street h", "street h","street h", "street h", "street h", "street h", "little village", "little village", "little village", "grass"},
+            {"wood","grass","street v","wood", "grass", "grass", "wood", "grass", "grass", "wood", "grass","wood", "street v", "street v", "wood", "grass", "grass", "wood","grass", "grass", "wood", "grass", "little village", "little village", "little village", "wood"},
+            {"grass","grass","street v", "grass", "wood", "grass", "grass", "grass", "wood", "water","water", "wood", "street v", "street v", "wood", "water", "water", "wood","grass", "wood", "grass", "grass", "grass", "street v", "grass", "wood"},
+            {"grass","grass","street v", "grass", "grass", "grass", "grass", "wood", "water", "water","water", "wood", "street v", "street v", "wood", "water", "water", "water","wood", "grass", "grass", "grass", "grass", "street v", "grass", "grass"},
+            {"water","water","bridge v", "water", "water", "water", "water", "water", "water", "water","water", "grass", "street v", "street v", "grass", "water", "water", "water","water", "water", "water", "water", "water", "bridge v", "water", "water"},
+            {"water","water","bridge v", "water", "water", "water", "water", "water", "water", "water","grass", "grass", "street v", "street v", "grass", "grass", "water", "water","water", "water", "water", "water", "water", "bridge v", "water", "water"},
+            {"water","water","bridge v", "water", "water", "water", "water", "water", "water", "water","wood", "grass", "street v", "street v", "grass", "wood", "water", "water","water", "water", "water", "water", "water", "bridge v", "water", "water"},
+            {"water","water","bridge v", "water", "water", "water", "water", "water", "water", "water","water", "grass", "street v", "street v", "grass", "water", "water", "water","water", "water", "water", "water", "water", "bridge v", "water", "water"},
+            {"grass","grass","street v", "grass", "wood", "wood", "wood", "grass", "water", "water","water", "wood", "street v", "street v", "wood", "water", "water", "water","grass", "wood", "wood", "wood", "grass", "street v", "grass", "grass"},
+            {"grass","grass","street v", "grass", "grass", "grass", "grass", "grass", "grass", "water","water", "wood", "street v", "street v", "wood", "water", "water", "grass","wood", "grass", "grass", "grass", "grass", "street v", "grass", "grass"},
+            {"wood","little village","little village", "little village", "grass", "grass", "grass", "grass", "grass", "grass","grass", "wood", "street v", "street v", "wood", "grass", "grass", "wood","grass", "grass", "grass", "grass", "grass", "street v", "grass", "wood"},
+            {"wood","little village","little village", "little village", "street h", "street h", "street h", "street h", "street h", "street h","street h", "street h", "street h", "street h", "street h", "street h", "street h", "street h","street h", "street h", "street h", "street h", "street h", "little village", "grass", "wood"},
+            {"grass","grass","grass", "wood", "wood", "grass", "grass", "grass", "grass", "grass","grass", "wood", "wood", "wood", "wood", "grass", "grass", "grass","grass", "grass", "grass", "wood", "wood", "grass", "grass", "grass"}
+
     };
 }
