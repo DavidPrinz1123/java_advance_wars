48fe90351e9d92ee347c45a8c934d5065c54715a
Tobias Stoppelkamp <stoppelkamp05@gmail.com>
2024-06-05 11:31:09 +0200

    more units

---
+++ src/main/java/org/apps/advancewars/terrain/TerrainType.java
@@ -0,0 +1,18 @@
+/*package org.apps.advancewars.terrain;
+
+public enum TerrainType {
+    PLAIN("grass.png"),
+    WOOD("wood.png"),
+    MOUNTAIN("mountain.png"),
+    SEA("water.png");
+
+    private final String imagePath;
+
+    TerrainType(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+}*/
