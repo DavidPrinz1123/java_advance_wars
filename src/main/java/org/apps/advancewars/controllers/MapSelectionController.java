74bb2c349af8ebb478727185560078125e922d5b
Tobias Stoppelkamp <stoppelkamp05@gmail.com>
2024-06-06 11:54:16 +0200

    map selection controller fix

--- src/main/java/org/apps/advancewars/controllers/MapSelectionController.java
+++ src/main/java/org/apps/advancewars/controllers/MapSelectionController.java
@@ -2,15 +2,10 @@ package org.apps.advancewars.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import org.apps.advancewars.MainApp;
 
 public class MapSelectionController {
-
-    @FXML
-    private ImageView mapImageView;
 
     @FXML
     private Button eonSpringsButton;
@@ -39,6 +34,7 @@ public class MapSelectionController {
 
     private void selectMap(String mapName) {
         MainApp.showGameScene(mapName);
-        ((Stage) mapImageView.getScene().getWindow()).close(); // Close the map selection window
+        Stage stage = (Stage) eonSpringsButton.getScene().getWindow();
+        stage.close(); // Close the map selection window
     }
 }
