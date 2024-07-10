94afb780a59401c0a47fcd17f934b86fe95ce2d2
Tobias Stoppelkamp <stoppelkamp05@gmail.com>
2024-07-10 19:11:30 +0200

    exit game button

--- src/main/java/org/apps/advancewars/controllers/GameHUDController.java
+++ src/main/java/org/apps/advancewars/controllers/GameHUDController.java
@@ -1,8 +1,12 @@
 package org.apps.advancewars.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 public class GameHUDController {
 
     @FXML
@@ -11,6 +15,9 @@ public class GameHUDController {
 
     @FXML
     private VBox teamBlueInfo;
+
+    @FXML
+    private Button exitGameButton;
 
 
     public void updateTeamRedInfo(String info) {
@@ -27,4 +34,9 @@ public class GameHUDController {
         Label infoLabel = new Label(info);
         teamBlueInfo.getChildren().add(infoLabel);
     }
+    public void handleExitGame(ActionEvent event) {
+        Stage stage = (Stage) exitGameButton.getScene().getWindow();
+        stage.close();
+    }
 }
+
